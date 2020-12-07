eccc41f1e1b18ea1d4bc4b7c9960d569a33ba44b
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-12-07 22:42:11 +0300

    Redactor markup and add it to Main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Redactor from './Redactor';
+import Readctor from './Redactor';
 
 function Main() {
   return (
-    <>
-    </>
+    <main className="main">
+      <Redactor />
+    </main>
   )
 }
 export default Main;
--- src/components/Redactor.js
+++ src/components/Redactor.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function Redactor() {
   return (
-    <>
-    </>
+    <section className="redactor">
+      <h1 className="redactor__title">Let's create new banner!</h1>
+    </section>
   )
 }
 export default Redactor;
