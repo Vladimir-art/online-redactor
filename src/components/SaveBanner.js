aa9b0c91e3d0e3595f012a6f5c40cbe9f9866851
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-12-13 16:39:00 +0300

    create download

--- src/components/SaveBanner.js
+++ src/components/SaveBanner.js
@@ -7,7 +7,7 @@ import html2canvas from 'html2canvas';
 function SaveBanner() {
 
   function saveSectionAsImage(e) {
-    html2canvas(document.querySelector('.constructor'))
+    html2canvas(document.querySelector('.constructor'), { proxy: false, useCORS: true })
       .then((canvas) => {
         const linkTag = e.target.parentElement;
         linkTag.href = canvas.toDataURL('image/png');
@@ -19,15 +19,15 @@ function SaveBanner() {
 
   return (
     <section className="save">
-      <a  className="save__download" >
+      <a className="save__download" >
         <img className="save__image" src={file} alt="PNG" onClick={saveSectionAsImage} />
       </a>
-      <a href="/" className="save__download" >
+      <button className="save__download" >
         <img className="save__image" src={html} alt="HTML" />
-      </a>
-      <a href="/" className="save__download" >
+      </button>
+      <button className="save__download" >
         <img className="save__image" src={json} alt="json" />
-      </a>
+      </button>
     </section>
   )
 }
