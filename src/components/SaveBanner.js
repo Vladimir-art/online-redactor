77a3239c757e3780f917626842d0b3d963cdeee5
Vladimir Ermolaev <vl.ermolaevv@yandex.ru>
2020-12-13 18:02:30 +0300

    create clipboard html

--- src/components/SaveBanner.js
+++ src/components/SaveBanner.js
@@ -17,12 +17,31 @@ function SaveBanner() {
       .catch((err) => console.log(err));
   }
 
+  function CopyToClipboard() {
+    // Create a new textarea element and give it id='t'
+    let textarea = document.createElement('textarea');
+    textarea.id = 't';
+    // Optional step to make less noise on the page, if any!
+    textarea.style.height = 0;
+    // Now append it to your page somewhere, I chose <body>
+    document.body.appendChild(textarea);
+    // Give our textarea a value of whatever inside the div of id=containerid
+    textarea.value = document.querySelector('.constructor').innerHTML;
+    // Now copy whatever inside the textarea to clipboard
+    let selector = document.querySelector('#t');
+    selector.select();
+    document.execCommand('copy');
+    // Remove the textarea
+    document.body.removeChild(textarea);
+    alert('copied successfully as HTML!');
+  }
+
   return (
     <section className="save">
       <a className="save__download" >
         <img className="save__image" src={file} alt="PNG" onClick={saveSectionAsImage} />
       </a>
-      <button className="save__download" >
+      <button className="save__download" onClick={CopyToClipboard} >
         <img className="save__image" src={html} alt="HTML" />
       </button>
       <button className="save__download" >
